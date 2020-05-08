ce40e2aa0b297bc36f25d7daaa4918d0fb6ed1ea
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-05-08 11:46:00 -0600

    MVP NOT MET

--- data/Routers/projects-router.js
+++ data/Routers/projects-router.js
@@ -36,6 +36,79 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// router.get(':id/actions', (req, res) => {
+//     Projects.getProjectActions(req.body.projectId)
+//     .then(actions => {
+//         if (actions) {
+//             res.status(200).json({ data: actions })
+//         } else {
+//             res.status(404).json({
+//                 message: 'The action with the specified ID does not exist.'
+//             })
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({
+//             message: 'The actions could not be retrieved'
+//         });
+//     });
+// });
+
+
+// router.post('/', (req, res) => {
+//     Projects.insert(req.body)
+//     .then(project => {
+//         res.status(201).json(project);
+//     })
+//     .catch(err => {
+//         res.status(500).json({
+//             message: 'There was an error while saving the project to the database'
+//         });
+//     });
+// });
+
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({
+                message: 'The projects has been deleted'
+            })
+        } else {
+            res.status(404).json({
+                message: 'The project with the specified id cannot be found'
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error removing the project'
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    if(!req.body.name || !req.body.description){
+        res.status(400).json({
+            message: 'Please edit the name or description'
+        })
+    } else {
+        Projects.update(req.params.id, req.body)
+        .then(res.status(201).json({
+            message: 'Project updated successfully'
+        }))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Error updating project'
+            });
+        });
+    };
+});
+
+
 
 
 
